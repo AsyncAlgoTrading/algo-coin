ccb0504238138372d835448235c553bea470e3e7
Tim Paine <t.paine154@gmail.com>
2016-11-14 16:39:30 -0500

    adding test skeletons.

---
+++ tests/test_risk.py
@@ -0,0 +1,14 @@
+class TestRisk:
+    def setup(self):
+        print("TestUM:setup() before each test method")
+
+    def teardown(self):
+        print("TestUM:teardown() after each test method")
+
+    @classmethod
+    def setup_class(cls):
+        print("setup_class() before any methods in this class")
+
+    @classmethod
+    def teardown_class(cls):
+        print("teardown_class() after any methods in this class")
