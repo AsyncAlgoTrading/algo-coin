131b96efe6bcb27882d5d417406738be1068aec2
Tim Paine <t.paine154@gmail.com>
2016-11-20 18:06:30 -0500

    starting on exchange tests.

--- tests/test_exchange.py
+++ tests/test_exchange.py
@@ -1,5 +1,4 @@
-# for accurate coverage
-import exchange
+from mock import patch, MagicMock
 
 
 class TestExchange:
@@ -20,3 +19,31 @@ class TestExchange:
     def teardown_class(cls):
         pass
         # teardown_class() after any methods in this class
+
+    def test_init(self):
+        from config import ExchangeConfig
+        from exchange import Exchange
+
+        ec = ExchangeConfig()
+        e = Exchange(ec)
+        assert e
+
+    def test_receive(self):
+        from config import ExchangeConfig
+        from exchange import Exchange
+
+        ec = ExchangeConfig()
+        e = Exchange(ec)
+        assert e
+
+        e.ws = MagicMock()
+
+        with patch('json.loads') as m1:
+            for i, val in enumerate(['match',
+                                     'received',
+                                     'open',
+                                     'done',
+                                     'change',
+                                     'error']):
+                m1.return_value = {'type': val, 'sequence': i}
+                e._receive()
