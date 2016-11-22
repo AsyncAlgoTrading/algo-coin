681120f7166883fbea72ee26e94b991e4efcf346
Tim Paine <t.paine154@gmail.com>
2016-11-21 23:16:41 -0500

    adding tests for main.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,4 @@
-# for accurate coverage
-import main
+from mock import patch
 
 
 class TestMain:
@@ -21,6 +20,28 @@ class TestMain:
         pass
         # teardown_class() after any methods in this class
 
-    def test_main(self):
-        pass
+    def test_parse_command_line(self):
+        from main import parse_command_line
+        from enums import TradingType
 
+        argv = ['live']
+        ret = parse_command_line(argv)
+        assert ret.type == TradingType.LIVE
+
+        argv = ['sandbox']
+        ret = parse_command_line(argv)
+        assert ret.type == TradingType.SANDBOX
+
+        argv = ['backtest']
+        ret = parse_command_line(argv)
+        assert ret.type == TradingType.BACKTEST
+
+        argv = ['verbose']
+        ret = parse_command_line(argv)
+        assert ret.verbose
+
+    def test_main(self):
+        from main import main
+        with patch('main.TradingEngine'), \
+                patch('main.SMACrossesStrategy'):
+            main([])
