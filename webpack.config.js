856d2a0d3189368529b3ab1f577d20e280d9d401
Tim Paine <t.paine154@gmail.com>
2019-02-01 15:07:47 -0500

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
-var path = require('path');
+const path = require("path");
+const PerspectivePlugin = require("@jpmorganchase/perspective/webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './build/index.js',
-  output: {
-    path: __dirname + '/algocoin/ui/assets/static/js/',
-    filename: 'bundle.js',
-    publicPath: './static/js/'
-  },
-  module: {
-    rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.png$/, use: 'file-loader' }
-    ]
-  }
+    entry: './build/index.js',
+    output: {
+        path: __dirname + '/algocoin/assets/static/js/',
+        filename: 'bundle.js',
+        publicPath: './static/js/'
+    },
+    // resolveLoader: {
+    //     alias: {
+    //         "file-worker-loader": "@jpmorganchase/perspective-webpack-plugin/src/js/file_worker_loader.js"
+    //     }
+    // },
+    // resolve: {
+    //     extensions: [".ts", ".js", ".json"]
+    // },
+    plugins: [new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /(en|es|fr)$/), new PerspectivePlugin()],
+    module: {
+        rules: [
+            {test: /\.css$/, use: [{loader: 'style-loader', }, {loader: 'css-loader', }, ], },
+            {test: /\.ts?$/, loader: "ts-loader"}
+        ]
+    }
 };
